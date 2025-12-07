935ce92d657d12575e0b05b2e78479a50aebabbc
hen.faibish <hen.faibish@zoominfo.com>
2025-12-07 08:59:43 +0200

    feat: grw-13498

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,7 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    provideAnimations(),
-    provideRouter(routes),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
